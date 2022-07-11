333b6efab50aee28956df509519bf1de5d141da6
Thiago Galhardo <thiago.galhardo@hotmail.com>
2022-07-11 13:28:08 -0300

    install breeze

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+
+require __DIR__ . '/auth.php';
+
 
 Route::get('/', function () {
     return view('welcome');
-})->name('home');
-
+});
 
 Route::get('/login/admin', [AdminController::class, 'login'])->name('admin.login');
 
@@ -15,6 +16,3 @@ Route::get('/cadastro', [UserController::class, 'create'])->name('users.create')
 Route::post('/cadastro', [UserController::class, 'store'])->name('users.store');
 Route::get('/cadastro/erro', [UserController::class, 'create_error'])->name('users.create_error');
 Route::get('/cadastro/sucesso', [UserController::class, 'create_success'])->name('users.create_success');
-
-
-
