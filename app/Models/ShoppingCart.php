94f00fd7ce84b1036912828742c93798fa382a1f
Tatiana Moreno <tatiana.emilia.morenno@gmail.com>
2022-07-31 14:46:16 -0300

    Criando model eloquent do carrinho

---
+++ app/Models/ShoppingCart.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class ShoppingCart extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+
+    protected $table = 'shopping_cart';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'quantity',
+    ];
+    
+}
